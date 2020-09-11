caafa8cdd50d1205c61f2113f63e7de573d3bfda
Siva Selvan <ssiva.eng@gmail.com>
2020-09-11 14:00:19 +0530

    Test

--- src/View/Footer.js
+++ src/View/Footer.js
@@ -5,9 +5,31 @@ function Footer() {
     return (
         <div>
             <p id="para">C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</p>
-            <div id="footers">CHEMICALS &| PROCESS POWER WATER |& WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL</div>
+            <div id="footers">
+                <ul >
+                    <li className="list">CHEMICALS</li>
+                    <li className="list">PROCESS POWER WATER </li>
+                    <li className="list">WASTE WATER OILS</li>
+                    <li className="list">GAS PHARMA SUGARS</li>
+                    <li className="list">DISTILLERIES PAPER</li>
+                    <li className="list">PULP MARINE</li>
+                    <li className="list">DEFENCE METAL</li>
+                    <li className="list">MINING FOOD</li>
+                    <li className="list">BEVERAGE PETROCHEMICAL</li>
+                    <li className="list">REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE </li>
+                    <li className="list">RESIDENTIAL</li>
+                </ul> 
+            </div>
             <div id="example3">
-                <p>A red shadow.</p>
+                <div className="row">
+                    <div className="col">
+
+                    </div>
+                    <div className="col-2">
+
+                    </div>
+                    
+                </div>
             </div>
         </div>
 
