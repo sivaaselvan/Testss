16ce3da8d30fbf355a229aef10537fbc4a150385
Siva Selvan <ssiva.eng@gmail.com>
2020-09-11 21:21:15 +0530

    Test-6

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import '../App.css';
+import cal from '../assert/call.png';
 
 
 function Footer() {
     return (
         <div>
-            <p id="para">C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</p>
-            <div id="footers">
-                <ul >
-                    <li className="list">CHEMICALS</li>
-                    <li className="list">PROCESS POWER WATER </li>
-                    <li className="list">WASTE WATER OILS</li>
-                    <li className="list">GAS PHARMA SUGARS</li>
-                    <li className="list">DISTILLERIES PAPER</li>
-                    <li className="list">PULP MARINE</li>
-                    <li className="list">DEFENCE METAL</li>
-                    <li className="list">MINING FOOD</li>
-                    <li className="list">BEVERAGE PETROCHEMICAL</li>
-                    <li className="list">REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE </li>
-                    <li className="list">RESIDENTIAL</li>
-                </ul>
-            </div>
-            <div id="example3">
-               
+            <div className="boxs">
+                <div className="row-1">
+                    <div className="col-3">
+                        <h3><img src={cal} alt="caller" className="call"/></h3>
+                        <h2 className="toll">Toll free<b> 1800 200 1234</b></h2>
+                    </div>
+                    <div className="col-4">
+                        <h3><img src={cal} alt="caller" className="call"/></h3>
+                        <h2 className="toll">Toll free<b> 1800 200 1234</b></h2>
+                    </div>
+                    <div className="col-5">
+                        <h3><img src={cal} alt="caller" className="call"/></h3>
+                        <h2 className="toll">Toll free<b> 1800 200 1234</b></h2>
+                    </div>
+                   
 
-                
+                </div>
             </div>
         </div>
 
